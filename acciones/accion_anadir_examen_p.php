17d75df0d953494beb8cbebc01fe6655c4ad3833
Gonza116 <Gonza116@users.noreply.github.com>
2018-05-29 22:21:12 +0200

    Validación de oportunidades

--- acciones/accion_anadir_examen_p.php
+++ acciones/accion_anadir_examen_p.php
@@ -24,8 +24,11 @@
         if(getAge($_REQUEST['fecha_ex_p'], $cump) < 18){
             $error['edad'] = "El alumno no puede ser menor de edad en la fecha del examen";
         }
+        if($alumno['OPORTUNIDADES'] <= 0){
+            $error['opor'] = "El alumno no puede hacer un examen si no le quedan oportunidades";
+        }
         if(isset($error)){
-            $error['donde'] = "El error ha ocurrido en el formulario para añadir un nuevo examen práctico";
+            $error['donde'] = "<strong>El error ha ocurrido en el formulario para añadir un nuevo examen práctico</strong>";
             $_SESSION['error'] = $error;
             header("Location: ../lista_alumnos.php");
         } else {
--- acciones/accion_anadir_examen_t.php
+++ acciones/accion_anadir_examen_t.php
@@ -1,18 +1,24 @@
 <?php
+    session_start();
     require_once("../utiles/gestionDB.php");
     require_once("../utiles/gestionarAlumnos.php");
 
     $conexion = crearConexionBD();
 
     if(isset($_REQUEST['id'])){
+        $alumno = consultaAlumno($conexion, $_REQUEST['id']);
         if(trim($_REQUEST['id'], " ") == ""){
             $error['id'] = "El id del alumno no puede estar vacío";
         }
         if(trim($_REQUEST['fecha_ex_t'], " ") == ""){
             $error['fecha'] = "La fecha del examen no puede estar vacía";
         }
+        if($alumno['OPORTUNIDADES'] <= 0){
+            echo "hola";
+            $error['opor'] = "El alumno no puede hacer un examen si no le quedan oportunidades";
+        }
         if(isset($error)){
-            $error['donde'] = "El error ha ocurrido en el formulario para añadir un nuevo examen práctico";
+            $error['donde'] = "<strong>El error ha ocurrido en el formulario para añadir un nuevo examen práctico</strong>";
             $_SESSION['error'] = $error;
             header("Location: ../lista_alumnos.php");
         } else {
